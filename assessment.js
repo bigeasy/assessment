324d64ad0a772bc6c1ab6372231b88bf22a9cf12
Alan Gutierrez <alan@prettyrobots.com>
2016-01-29 01:15:24 -0600

    Add min and max.

--- assessment.js
+++ assessment.js
@@ -1,7 +1,16 @@
-function Window (interval, clock) {
-    this.clock = clock
+var RBTree = require('bintrees').RBTree
+
+function compare (a, b) {
+    return a.value - b.value || a.index - b.index
+}
+
+function Window (interval, options) {
+    this.index = 0
     this.interval = interval
     this.count = 0
+    this.Date = options.Date || Date
+    this.empty = ('empty' in options) ? options.empty : null
+    this.tree = new RBTree(compare)
     this.sum = 0
     this.head = { head: true, value: null }
     this.head.next = this.head.previous = this.head
@@ -11,13 +20,15 @@ Window.prototype.sample = function (value) {
     this.count++
     this.sum += value
     var node = {
-        when: this.clock(),
+        when: this.Date.now(),
         value: value,
+        index: this.index++,
         next: this.head.next,
         previous: this.head
     }
     node.previous.next = node
     node.next.previous = node
+    this.tree.insert(node)
     var node
     while (node.when - this.head.previous.when > this.interval)  {
         node = this.head.previous
@@ -25,20 +36,31 @@ Window.prototype.sample = function (value) {
         this.head.previous.next = this.head
         this.count--
         this.sum -= node.value
+        this.tree.remove(node)
     }
 }
 
 
-Window.prototype.calculate = function () {
-    return { average: this.count == 0 ? null : this.sum / this.count }
+Window.prototype.summarize = function () {
+    if (this.count == 0) {
+        return {
+            mean: this.empty,
+            min: this.empty,
+            max: this.empty
+        }
+    } else {
+        return {
+            mean: this.sum / this.count,
+            min: this.tree.min().value,
+            max: this.tree.max().value
+        }
+    }
 }
 
 function Assessment (options) {
     options || (options = {})
-    options.intervals || (options.intervals = [ 60000 ])
-    options.clock || (options.clock = function () { return Date.now() })
-    this.windows = options.intervals.map(function (interval) {
-        return new Window(interval, options.clock)
+    this.windows = (options.intervals || [ 60000 ]).map(function (interval) {
+        return new Window(interval, options)
     })
 
 }
@@ -50,11 +72,11 @@ Assessment.prototype.sample = function (value) {
     }
 }
 
-Assessment.prototype.calculate = function () {
+Assessment.prototype.summarize = function () {
     var intervals = {}
-    this.windows.forEach(function (window) {
-        intervals[window.interval] = window.calculate()
-    })
+    for (var i = 0, I = this.windows.length; i < I; i++) {
+        intervals[this.windows[i].interval] = this.windows[i].summarize()
+    }
     return intervals
 }
 
--- t/assessment/assessment.t.js
+++ t/assessment/assessment.t.js
@@ -4,13 +4,17 @@ function prove (assert) {
     var Assessment = require('../..')
     new Assessment().sample(0)
     var time = 1
-    var assessment = new Assessment({ intervals: [ 6000 ], clock: function () { return time ++ } })
-    assert(assessment.calculate(),  { '6000': { average: null } }, 'null')
+    var assessment = new Assessment({
+        empty: 0,
+        intervals: [ 6000 ],
+        Date: { now: function () { return time++ } }
+    })
+    assert(assessment.summarize(),  { '6000': { mean: 0, min: 0, max: 0 } }, 'null')
     assessment.sample(1)
     assessment.sample(2)
     assessment.sample(3)
-    assert(assessment.calculate(), { '6000': { average: 2 } }, 'calculate')
+    assert(assessment.summarize(), { '6000': { mean: 2, min: 1, max: 3 } }, 'summarize')
     time = 6002
     assessment.sample(4)
-    assert(assessment.calculate(), { '6000': { average: 3 } }, 'shift')
+    assert(assessment.summarize(), { '6000': { mean: 3, min: 2, max: 4 } }, 'shift')
 }
