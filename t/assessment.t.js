7c5e3d37b4526d8b94f1db2e02468e2417eae7ed
Alan Gutierrez <alan@prettyrobots.com>
2018-07-29 19:37:18 -0500

    Rename `assert` to `okay`.

--- t/assessment.t.js
+++ t/assessment.t.js
@@ -1,6 +1,6 @@
 require('proof')(5, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Assessment = require('..')
     new Assessment().sample(0)
     var time = 1
@@ -9,16 +9,16 @@ function prove (assert) {
         intervals: [ 6000 ],
         Date: { now: function () { return time++ } }
     })
-    assert(assessment.summarize(),  { '6000': { mean: 0, median: 0, min: 0, max: 0 } }, 'null')
+    okay(assessment.summarize(),  { '6000': { mean: 0, median: 0, min: 0, max: 0 } }, 'null')
     assessment.sample(1)
     assessment.sample(2)
     assessment.sample(3)
-    assert(assessment.summarize(), { '6000': { mean: 2, median: 2, min: 1, max: 3 } }, 'summarize')
+    okay(assessment.summarize(), { '6000': { mean: 2, median: 2, min: 1, max: 3 } }, 'summarize')
     time = 6002
     assessment.sample(4)
-    assert(assessment.summarize(), { '6000': { mean: 3, median: 3, min: 2, max: 4 } }, 'shift')
+    okay(assessment.summarize(), { '6000': { mean: 3, median: 3, min: 2, max: 4 } }, 'shift')
     assessment.sample(1)
-    assert(assessment.summarize(), {
+    okay(assessment.summarize(), {
         '6000': { mean: 2.6666666666666665, median: 3, min: 1, max: 4 }
     }, 'median add odd')
     time = 12003
@@ -26,7 +26,7 @@ function prove (assert) {
     assessment.sample(3)
     assessment.sample(2)
     assessment.sample(1)
-    assert(assessment.summarize(), {
+    okay(assessment.summarize(), {
         '6000': { mean: 2.5, median: 2, min: 1, max: 4 }
     }, 'median remove even')
     time = 18004
